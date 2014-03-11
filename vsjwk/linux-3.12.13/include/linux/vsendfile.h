6af6597678d4d65030317461009e7bc71b17816c
JWK <kjw0402@naver.com>
2014-03-11 23:06:06 +0900

    change

--- vsjwk/linux-3.12.13/include/linux/vsendfile.h
+++ vsjwk/linux-3.12.13/include/linux/vsendfile.h
@@ -4,6 +4,6 @@
 #include <linux/unistd.h>
 #include <linux/linkage.h>
 
-_syscall2(long, vsendfile, char*, arg1, char*, arg2);
+_syscall2(long, vsendfile, char*, source, char*, destination);
 
 #endif
